8ffb97b786319525c1768682809d51561538d7a6
manu aggarwal <manuaggarwal2001@gmail.com>
2023-11-25 19:02:26 +0530

    github change

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 require('dotenv').config()
 const express = require("express")
 const app = express()
+const githubData = {
+    "manu":"aggarwal",
+    "kirti":"aggarwal",
+    "anju":"agg"
+}
 app.get('/',(req,res)=>{
 res.send("helllo everyone")
+})
+app.get('/github',(req,res)=>{
+    res.json(githubData)
 })
 app.listen(process.env.PORT,()=>{
     console.log(`servering running on ${process.env.PORT}`)
